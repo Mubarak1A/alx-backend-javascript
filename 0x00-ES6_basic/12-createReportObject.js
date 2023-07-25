77edb4a88183d10b84ff8ce33cab7b2e3fae8150
Mubarak1A <adesinamubarak123@example.com>
2023-07-25 02:03:54 +0100

    create report object

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,13 @@
+export default function createReportObject(employeesList) {
+    const reportObject = {
+      allEmployees: {
+        ...employeesList,
+      },
+      getNumberOfDepartments: function () {
+        return Object.keys(employeesList).length;
+      },
+    };
+  
+    return reportObject;
+  }
+  
