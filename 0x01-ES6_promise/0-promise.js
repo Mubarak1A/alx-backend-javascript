7371834bdb192dd1115bbc64d8bcc82d8d22b586
Mubarak1A <adesinamubarak123@example.com>
2023-07-29 18:07:59 +0100

    update function

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,3 @@
 function getResponseFromAPI(){
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve("Fulfilled")
-        }, 100);
-    })
+    return new Promise((resolve, reject) => {})
 }
