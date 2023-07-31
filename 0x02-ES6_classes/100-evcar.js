8c9a3223c964be4387591454a827fa6036c97313
Mubarak1A <adesinamubarak123@example.com>
2023-07-31 18:41:55 +0100

    keep privacy

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,30 @@
+/* Import Car from 10-car.js.
+ * Implement a class named EVCar that extends the Car class:
+ * Constructor attributes: brand (String), motor (String), color (String), range (String)
+ * Each attribute must be stored in an “underscore” attribute version (ex: name is stored in _name)
+ * For privacy reasons, when cloneCar is called on a EVCar object,
+ * the object returned should be an instance of Car instead of EVCar.
+ */
+
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this.range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  set range(value) {
+    this._range = value;
+  }
+
+  cloneCar() {
+    const Species = super.constructor[Symbol.species];
+
+    return new Species();
+  }
+}
