d9288e28c956a652035228042fe7d98239e54bae
Mubarak1A <adesinamubarak123@example.com>
2023-07-31 17:57:58 +0100

    create obj from class function

---
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -0,0 +1,14 @@
+/* Import the ClassRoom class from 0-classroom.js.
+ * Implement a function named initializeRooms.
+ * It should return an array of 3 ClassRoom objects with the sizes 19, 20, and 34 (in this order).
+ */
+
+import Classroom from './0-classroom';
+
+export default function initializeRooms() {
+  return [
+    new Classroom(19),
+    new Classroom(20),
+    new Classroom(34),
+  ];
+}
